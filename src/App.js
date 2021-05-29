b3e2a29369736aee4486fc7a2f581b2d5c6e6f72
shaikwasef <wasefshaik@gmail.com>
2021-05-29 22:35:01 +0530

    add project

--- src/App.js
+++ src/App.js
@@ -1,58 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useEffect } from 'react';
+import { applicationABI, applicationAddress } from '../../to-do-list-bc/src/utils/config';
+import fetchTodoItems from '../../to-do-list-bc/src/utils/fetchInCompleteTodoItems';
+import makeConnection from '../../to-do-list-bc/src/utils/makeConnection';
+import Banner from "./components/Banner"
+import Todo from "./components/Todo"
 import './App.css';
 
 function App() {
+
+  
+  useEffect(() => {
+      async function initialSetup(){
+      const {web3,account} = await  makeConnection();
+      const abi = applicationABI;
+      const address = applicationAddress;
+      let {methods} = new web3.eth.Contract(abi,address);
+      const todoItems = await fetchTodoItems(methods);
+      const balance = await web3.eth.getBalance(account);
+      //balance
+      const wei =  1000000000000000000;
+      console.log((balance/wei).toFixed(4));
+      //todo items
+      console.log(todoItems);
+    }
+    initialSetup();
+    },[]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="app">
+      <Banner/>
+      <Todo/>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+//getter for block chain
+// let {methods} = new web3.eth.Contract(abi,address);
+// async function fetchToDoItems(){
+// const taskCount =  await methods.taskCount().call();
+// for(let i = 1 ; i <= taskCount ; i++){
+//   const task = await methods.tasks(i).call();
+//   console.log(task);
+//   }
+// }
+// fetchToDoItems();
+
+
+// Add something on the block chain
+    //   const makeChanges = async () => {
+    //     try{
+    //       const accounts = await  web3.eth.getAccounts();
+    //       const account = accounts[0];
+    //       methods.deposit(amount).send({from:account}).then((transaction) => console.log(transaction)).catch((error) => console.log(error));
+    //     }catch(error){
+    //       console.log(error)
+    //     }
+    // }
+    // makeChanges();
--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
+import { createStore } from 'redux';
+import {allReducers} from './reducers/index'
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { StylesProvider } from '@material-ui/core';
+
+const store = createStore(allReducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+    <StylesProvider injectFirst>
       <App />
+    </StylesProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
